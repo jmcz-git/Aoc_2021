69b747b89306e20d14295cd2333247a2d4675c58
Juan Miguel Carmona Zavala <jmcz@M1-Pro.local>
2021-12-01 13:53:07 -0700

    Day One [JavaScript]

---
+++ 1.js
@@ -0,0 +1,18 @@
+// part one
+// number of successive increments
+let solve_part_one = (lst) => (
+  lst.split('\n')
+     .map(s => Number(s))
+     .map((n,i,l) => n < l[i+1])
+     .reduce((p,c) => p + c))
+
+// part two
+// number of successive incremetns of the
+// "3-measurement sliding window"
+let solve_part_two = (lst) => (
+  lst.split('\n')
+     .map(s => Number(s))
+     .map((n,i,a) => i+2 < a.length ? n + a[i+1] + a[i+2] : null)
+     .filter(n => !(n === null))
+     .map((n,i,l) => n < l[i+1])
+     .reduce((p,c) => p + c))
